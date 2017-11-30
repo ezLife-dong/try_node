1a1b879d980749a50a829c92d9d637f612ec8c95
董凯 <dongkai01@meicai.cn>
2017-11-30 16:09:31 +0800

    add express

---
+++ node1st.js
@@ -0,0 +1,10 @@
+var http = require("http");
+// var express = require("express");
+http.createServer(function(request,response){
+	response.writeHead(200,{'Content-Type':'text/plain'});
+	response.end('hello node!\n');
+}).listen(8888);
+console.log('Server is running at http://127.0.0.1:8888！/');
+// console.log(express);
+// var ass = document.createTextNode('abs');
+// document.body.appendChild(ass);
