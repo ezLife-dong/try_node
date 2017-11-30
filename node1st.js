85b2a1b93c07d24f316d89a0a65de01784c86f6b
董凯 <dongkai01@meicai.cn>
2017-11-30 16:52:45 +0800

    1

--- node1st.js
+++ node1st.js
@@ -1,8 +1,9 @@
 var http = require("http");
-// var express = require("express");
+var express = require("express");
 http.createServer(function(request,response){
 	response.writeHead(200,{'Content-Type':'text/plain'});
 	response.end('hello node!\n');
+	// response.end(express);
 }).listen(8888);
 console.log('Server is running at http://127.0.0.1:8888！/');
 // console.log(express);
